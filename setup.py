2d26da9225327edd676d3c17b5226207c5a723df
Anagh Kanungo <anaghkanungo7@gmail.com>
2023-07-27 19:32:24 -0400

    Updated setup.py with install requirements

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='1.0.0',
     url='https://github.com/anaghkanungo7/mfindia',
     author='Anagh Kanungo',
-    author_email='kanungoanagh@gmail.com',
+    author_email='anaghkanungo7@gmail.com',
     description='Mutual Funds India Price Data',
     packages=find_packages(),    
-    install_requires=[],
+    install_requires=[bs4, pandas, requests, sqlalchemy],
 )
